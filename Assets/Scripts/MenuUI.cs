0923cb11ac1f78478208337c30532994a55368da
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2021-02-08 17:33:09 -0500

    Pause Menu

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Cinemachine;
+using TMPro;
 
 public class MenuUI : MonoBehaviour {
     
     public EnemySettings enemySettings;
     public CinemachineVirtualCamera uiCamera;
+    public TMP_Text menuTitle;
+    public TMP_Text newGameButtonText;
     public Button newGameButton;
     public Button videoSettingsButton;
     public Button audioSettingsButton;
@@ -17,6 +20,8 @@ public class MenuUI : MonoBehaviour {
     public Animator audioSettingsAnimator;
 
     float targetWeight;
+    bool gameStarted = false;
+    bool gamePaused = false;
 
     void Start() {
         newGameButton.onClick.AddListener(NewGame);
@@ -27,14 +32,47 @@ public class MenuUI : MonoBehaviour {
         enemySettings.targetWeight = 0;
         videoSettingsAnimator.Play("Closed");
         audioSettingsAnimator.Play("Closed");
+        menuTitle.text = "BULLET HELL";
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) && gameStarted) {
+            if (gamePaused) {
+                gamePaused = false;
+                uiCamera.Priority = 0;
+                animator.Play("Fade Out");
+                gameUI.SetActive(true);
+                playerMovement.move = true;
+                Time.timeScale = 1f;
+            } else {
+                gamePaused = true;
+                uiCamera.Priority = 20;
+                animator.Play("Fade In");
+                gameUI.SetActive(false);
+                playerMovement.move = false;
+                Time.timeScale = 0f;
+            }
+        }
     }
 
     public void NewGame() {
-        uiCamera.Priority = 0;
-        animator.Play("Fade Out");
-        playerMovement.move = true;
-        enemySettings.targetWeight = targetWeight;
-        gameUI.SetActive(true);
+        if (gameStarted) {
+            gamePaused = false;
+            uiCamera.Priority = 0;
+            animator.Play("Fade Out");
+            gameUI.SetActive(true);
+            playerMovement.move = true;
+            Time.timeScale = 1f;
+        } else {
+            uiCamera.Priority = 0;
+            animator.Play("Fade Out");
+            playerMovement.move = true;
+            enemySettings.targetWeight = targetWeight;
+            gameUI.SetActive(true);
+            gameStarted = true;
+            menuTitle.text = "PAUSED";
+            newGameButtonText.text = "RESUME";
+        }
     }
 
     public void VideoSettings() {
